f63b1254b2a8ab6df39d2a74ce7ffca8a38d47f6
Patrick <patrick.lopdrup@gmail.com>
2020-10-03 21:42:35 +0200

    Click in the grid to add a cell

--- path_finding/lib/canvas.dart
+++ path_finding/lib/canvas.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:path_finding/cell.dart';
 
+int rows = 15;
+int cols = 10;
+double dx, dy = -1;
+double padding = 10;
+Cell cell = Cell();
+List<Cell> cellList = List();
+
 class MyCanvas extends StatefulWidget {
   @override
   _MyCanvasState createState() => _MyCanvasState();
@@ -11,16 +18,25 @@ class _MyCanvasState extends State<MyCanvas> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.grey[700],
-      padding: EdgeInsets.all(20),
+      padding: EdgeInsets.all(padding),
       child: LayoutBuilder(
-        builder: (_, constraints) => Container(
-          // width and height is the same
-          width: constraints.maxWidth,
-          height: constraints.maxWidth,
-          color: Colors.white,
-          child: CustomPaint(
-            painter: MyPainter(),
+        builder: (_, constraints) => GestureDetector(
+          child: Container(
+            // width and height is the same
+            width: constraints.maxWidth,
+            height: constraints.maxWidth,
+            color: Colors.white,
+            child: CustomPaint(
+              painter: MyPainter(dx, dy),
+            ),
           ),
+          onTapDown: (details) {
+            setState(() {
+              dx = details.globalPosition.dx;
+              dy = details.globalPosition.dy;
+              print('hej $dx, $dy');
+            });
+          },
         ),
       ),
     );
@@ -28,39 +44,75 @@ class _MyCanvasState extends State<MyCanvas> {
 }
 
 class MyPainter extends CustomPainter {
-  int rows = 15;
-  int cols = 10;
-  Cell cell = Cell();
-  List<List<int>> grid = [];
+  double x, y;
+  MyPainter(this.x, this.y);
+
+  // Generate 2d list
+  List grid = List.generate(cols, (index) => List(rows));
 
   @override
   void paint(Canvas canvas, Size size) {
-    double myWidth = size.width / rows;
-    double myHeight = size.height / cols;
+    print("hello");
+    double cellWidth = size.width / cols;
+    double cellHeight = size.height / rows;
+
+    int cellX = (x - padding) ~/ cellWidth;
+    // Appbar is 80 pixel
+    int cellY = (y - padding - 80) ~/ cellHeight;
+
+    // Making grid of cells
+    for (int col = 0; col < cols; col++) {
+      for (int row = 0; row < rows; row++) {
+        grid[col][row] = Cell(x: col, y: row);
+      }
+    }
+
+    // Draw startCell
+    Cell startCell = grid[0][0];
+    canvas.drawRect(
+        Rect.fromLTWH(0, 0, cellWidth, cellHeight), startCell.startPaint());
+
+    // Draw endCell
+    Cell endCell = grid[cols - 1][rows - 1];
+    canvas.drawRect(
+        Rect.fromLTWH(endCell.x * cellWidth, endCell.y * cellHeight, cellWidth, cellHeight), endCell.endPaing());
+
+
+    Cell temp = grid[cellX][cellY];
+    print(temp);
+    // Adding cell to global list
+    cellList.add(temp);
+
+    // Drawing all cells in list
+    if (cellList.length > 0) {
+      for (cell in cellList) {
+        print('$cell');
+        canvas.drawRect(
+            Rect.fromLTWH(
+                cell.x * cellWidth, cell.y * cellHeight, cellWidth, cellHeight),
+            cell.paint());
+      }
+    }
 
     final drawGrid = Paint()
-    ..color = Colors.black
-    ..style = PaintingStyle.stroke
-    ..strokeWidth = 1;
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1;
 
-    // Making 2d array
-    for (int i = 0; i < cols; i++) {
-      grid.insert(i, List(rows));
-    }
-    
     // Draw rows
     for (int i = 0; i < rows; i++) {
-      canvas.drawLine(Offset(0, i*myWidth), Offset(size.height, i*myWidth), drawGrid);
+      canvas.drawLine(Offset(0, i * cellHeight),
+          Offset(size.height, i * cellHeight), drawGrid);
     }
     // Draw cols
     for (int j = 0; j < cols; j++) {
-      canvas.drawLine(Offset(j*myHeight, 0), Offset(j*myHeight, size.width), drawGrid);
+      canvas.drawLine(Offset(j * cellWidth, 0),
+          Offset(j * cellWidth, size.width), drawGrid);
     }
-
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
--- path_finding/lib/cell.dart
+++ path_finding/lib/cell.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class Cell {
@@ -7,9 +6,28 @@ class Cell {
   int fVal, gVal, hVal;
   double pctForWall = 20.0;
   bool isWall;
+  bool isGenerated = false;
 
-  Paint generateSquare() {
-    return calcIfWall(pctForWall) ? _wallPaint() : _normPaint();
+  // Constructor taking x- and y coordinates
+  Cell({this.x, this.y});
+
+  Paint paint() {
+    if (!this.isGenerated) {
+      this.isGenerated = true;
+      return _generateSquare();
+    } else {
+      return this.isWall ? _wallPaint() : _normPaint();
+    }
+  }
+
+  Paint _generateSquare() {
+    if (calcIfWall(pctForWall)) {
+      this.isWall = true;
+      return _wallPaint();
+    } else {
+      this.isWall = false;
+      return _normPaint();
+    }
   }
 
   bool calcIfWall(double pctForWall) {
@@ -19,7 +37,7 @@ class Cell {
 
   Paint _normPaint() {
     return Paint()
-      ..color = Colors.red[400]
+      ..color = Colors.purple
       ..style = PaintingStyle.fill;
   }
 
@@ -27,5 +45,23 @@ class Cell {
     return Paint()
       ..color = Colors.grey[700]
       ..style = PaintingStyle.fill;
+  }
+
+  Paint startPaint() {
+    return Paint()
+      ..color = Colors.green
+      ..style = PaintingStyle.fill;
+  }
+
+  Paint endPaing() {
+    return Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.fill;
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '$x,$y - wall: $isWall';
   }
 }
